3130096d18ffcf9e5ab004495b8b209d7eeab296
YangyangHuang <yangyang@weimill.com>
2025-02-11 15:33:02 +0800

    move

---
+++ server/src/module/system/guess/entities/guess.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('tw_guess')
+export class Guess {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ comment: '期号', length: 14 })
+  issueNumber: string
+
+  @Column({ comment: '开奖号码', type: 'int' })
+  number: number
+
+  @Column({ comment: '大小结果', length: 10 })
+  result: string
+
+  @CreateDateColumn({ comment: '创建时间', type: 'timestamp' })
+  createTime: Date
+}
